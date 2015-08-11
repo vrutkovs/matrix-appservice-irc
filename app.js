aa537848fbb6b2fcd7c4d07bcc6024a2cdb17cc0
Kegan Dougal <kegan@matrix.org>
2015-08-11 16:46:42 +0100

    Make the project compatible with 0.2.0 of matrix-appservice
    This simplifies startup logic considerably.

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 "use strict";
-var crc = require("crc");
-var crypto = require("crypto");
 var yaml = require("js-yaml");
 var fs = require("fs");
 var nopt = require("nopt");
 
 var Validator = require("./lib/config/validator");
-
-// when invoked with 'node app.js', make an AS with just the IRC service.
-var appservice = require("matrix-appservice");
+var AppServiceRegistration = require("matrix-appservice").AppServiceRegistration;
 var irc = require("./lib/irc-appservice.js");
 var hotReload = require("./lib/hot-reload.js");
 
@@ -71,49 +67,30 @@ if (!config) {
     process.exit(1);
     return;
 }
-config.appService.service = irc;
-config.appService.generateRegistration = Boolean(opts["generate-registration"]);
-config.appService.skipCrcCheck = Boolean(opts["skip-crc-check"]);
 config.logging.verbose = Boolean(opts["verbose"]);
 
-// make a checksum of the IRC server configuration. This will be checked against
-// the checksum created at the last "--generate-registration". If there is a
-// difference, it means that the user has failed to tell the HS of the new
-// registration, so we can refuse to start until that is done.
-var checksum = crc.crc32(JSON.stringify(config.servers)).toString(16);
-var randomPart = crypto.randomBytes(32).toString('hex');
-config.appService.homeserver.token = randomPart + "_crc" + checksum;
-irc.configure(config);
-appservice.registerService(config.appService);
-
-if (config.appService.generateRegistration) {
-    var fname = "appservice-registration-irc.yaml";
-    console.log("Generating registration file to %s...", fname);
-    appservice.getRegistration().done(function(registration) {
-        fs.writeFile(fname, yaml.safeDump(registration), function(e) {
-            if (e) {
-                console.error("Failed to write registration file: %s", e);
-                return;
-            }
-            console.log(" "+Array(74).join("="));
-            console.log("   Generated registration file located at:");
-            console.log("       %s", fname);
-            console.log("");
-            console.log("   The HS token this service looks for has been"+
-                " updated. You MUST update");
-            console.log("   the HS even if config.yaml was not modified."+
-                " This file MUST be added");
-            console.log("   to the destination home "+
-                "server configuration file (e.g. 'homeserver.yaml'):");
-            console.log("");
-            console.log('       app_service_config_files: '+
-                '["appservice-registration-irc.yaml"]');
-            console.log(" "+Array(74).join("="));
-            process.exit(0);
-        });
+if (Boolean(opts["generate-registration"])) {
+    irc.generateRegistration(config).done(function(reg) {
+        var fname = "appservice-registration-irc.yaml";
+        reg.outputAsYaml(fname);
+        console.log(" "+Array(74).join("="));
+        console.log("   Generated registration file located at:");
+        console.log("       %s", fname);
+        console.log("");
+        console.log("   The HS token this service looks for has been"+
+            " updated. You MUST update");
+        console.log("   the HS even if config.yaml was not modified."+
+            " This file MUST be added");
+        console.log("   to the destination home "+
+            "server configuration file (e.g. 'homeserver.yaml'):");
+        console.log("");
+        console.log('       app_service_config_files: '+
+            '["appservice-registration-irc.yaml"]');
+        console.log(" "+Array(74).join("="));
+        process.exit(0);
     });
 }
 else {
     hotReload.setup();
-    appservice.runForever();
+    irc.runService(config, Boolean(opts["skip-crc-check"]));
 }
